95f053a67d10af07cc824dc6f93d671695e356dc
naraekn <34229459+naraekn@users.noreply.github.com>
2020-10-15 00:23:57 +0900

    Update a new answer for problem 02 on 2020/10/14

--- daily-challenge/201014_problem02.test.js
+++ daily-challenge/201014_problem02.test.js
@@ -4,6 +4,8 @@
  * 
  * [느낀 점]
  * 정규표현식을 공부하고 자야겠다.
+ * string에 대한 메서드 "repeat"을 사용하는 것도 좋은 풀이인거같다. 
+ * repeat을 사용하는 연습을 해야겠다.
  */
 
 function solution1(phone_number) {
@@ -18,8 +20,12 @@ function solution2(phone_number) {
   return phone_number.replace(/\d(?=\d{4})/g, "*");
 }
 
+function solution3(phone_number){
+  return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+}
+
 test('sample', () => {
-  [solution1, solution2].forEach(solution => {
+  [solution1, solution2, solution3].forEach(solution => {
       expect(
         solution("01033334444"))
         .toEqual("*******4444");   
